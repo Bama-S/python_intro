d81de3e75f02e7bea3e8ed09fc7ba0741f6a4438
Bama S <Bama-S@users.noreply.github.com>
2022-12-06 08:24:58 +0000

    converting_python3

--- ex1.py
+++ ex1.py
@@ -1,8 +1,9 @@
 #count the number of vowel in the string
-name = raw_input("Enter the string")
-print "The string you entered is: ", name
+name = input("Enter the string")
+
+print ("The string you entered is: ", name)
 count  = 0
 for s in name:
-    if s=="a" or s=="e" or s=="i" or s=="o" or s=="u":
+    if (s=="a" or s=="e" or s=="i" or s=="o" or s=="u"):
         count +=1
-print "The number of vowels: ", count
+print ("The number of vowels: ", count)
